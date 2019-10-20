5646f8658f8c965ee2e7a59417058b8171509626
GudochkinK <goodok-01@hotmail.com>
2019-10-20 16:36:51 +0300

    Конструкторы

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,15 +10,12 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-            Console.WriteLine("1");
-=======
             Point p1 = new Point(12,20,'*');
             p1.Draw();
 
             Point p2 = new Point(24, 15, '#');
             p2.Draw();
->>>>>>> Stashed changes
+
             Console.ReadKey();
         }
     }
