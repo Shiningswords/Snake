d4549330787633da225e7c68328d0d46d5393fd5
GudochkinK <goodok-01@hotmail.com>
2019-10-20 15:53:56 +0300

    Метод Draw

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -8,9 +8,21 @@ namespace Snake
 {
     class Program
     {
+        static void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(symb);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("1");
+            int x1 = 4;
+            int y1 = 10;
+            char symb1 = '*';
+            Draw(x1, y1, symb1);
+            int x2 = 3;
+            int y2 = 5;
+            char symb2 = '#';
+            Draw(x2, y2, symb2);
             Console.ReadKey();
         }
     }
